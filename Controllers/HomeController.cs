a989497d7ec233548a844c3cd903b4c93a7ed4fc
Madhu sudan Bhattarai <summerpalpali@gmail.com>
2023-07-23 14:34:04 +0545

    Authorize addes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,27 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using AuthWeb.Models;
+using AuthWeb.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AuthWeb.Controllers;
 
+[Authorize]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
     {
         _logger = logger;
+        _userManager = userManager;
     }
-
+     
     public IActionResult Index()
     {
+        ViewData["UserID"] = _userManager.GetUserId(this.User);
         return View();
     }
 
